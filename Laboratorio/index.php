7bf4151eb5c2971ffb9aab57d1736d8289dd627c
Storepac <38814895+Storepac@users.noreply.github.com>
2020-07-14 16:58:39 -0300

    Add files via upload

---
+++ Laboratorio/index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<title>Bioanalises Laboratório de Tupã</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="stylesheet" href="assets/css/main.css" />
+</head>
+
+<body>
+
+	<!-- Header -->
+	<header id="header">
+
+		<a href="index.html" class="logo">Bioanalises Laboratório</a>
+		<nav class="right">
+			<a href="../index.php" target="_blank" class="button alt">Login</a>
+		</nav>
+	</header>
+
+
+
+	<!-- Banner -->
+	<section id="banner">
+		<div class="content">
+			<h1>Consulte seus Exames</h1>
+			<p>Aqui você pode consultatr de maneira simples e fácil<br />seus exames e saber de tudo.</p>
+			<ul class="actions">
+				<li><a href="../index.php" target="_blank" class="button scrolly">Clique Aqui</a></li>
+			</ul>
+		</div>
+	</section>
+
+	<!-- One -->
+	<section id="one" class="wrapper">
+		<div class="inner flex flex-3">
+			<div class="flex-item left">
+				<div>
+					<h3>Hemograma</h3>
+					<p>Morbi in sem quis dui plalorem ipsum<br /> euismod in, pharetra sed ultricies.</p>
+				</div>
+				<div>
+					<h3>Beta HCG</h3>
+					<p>Exame de gravidez<br /> loreipsu gravida adipiscing lorem</p>
+				</div>
+			</div>
+			<div class="flex-item image fit round">
+				<img src="images/pic01.jpg" alt="" />
+			</div>
+			<div class="flex-item right">
+				<div>
+					<h3>Tempus nullam</h3>
+					<p>Sed adipiscing ornare risus. Morbi estes<br /> blandit sit et amet, sagittis magna.</p>
+				</div>
+				<div>
+					<h3>Suscipit nibh dolore</h3>
+					<p>Pellentesque egestas sem. Suspendisse<br /> modo ullamcorper feugiat lorem.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Two -->
+	<section id="two" class="wrapper style1 special">
+		<div class="container">
+		<div class="row">
+			<div class="flex-item left">
+				<div class="inner">
+					<h2>Unidade de Tupã</h2>
+					<figure>
+						<blockquote>
+							"Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra<br />
+							magna etiam lorem ultricies in diam. Sed arcu cras consequat."
+						</blockquote>
+						<footer>
+							<cite class="author">Jane Anderson</cite>
+							<cite class="company">CEO, Untitled</cite>
+						</footer>
+					</figure>
+				</div>
+			</div>
+			<div>
+				<div class="flex-item right">
+					<div class="inner">
+						<h2>Unidade de Herculândia</h2>
+						<figure>
+							<blockquote>
+								"Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra<br />
+								magna etiam lorem ultricies in diam. Sed arcu cras consequat."
+							</blockquote>
+							<footer>
+								<cite class="author">Jane Anderson</cite>
+								<cite class="company">CEO, Untitled</cite>
+							</footer>
+						</figure>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	</section>
+
+
+			<!-- Main -->
+			<section id="main" class="wrapper">
+				<div class="inner">
+					<header class="align-center">
+						<h1>Quem somos?</h1>
+						<p>Conheça um pouco mais da nossa história</p>
+					</header>
+					<div class="image fit">
+						<img src="images/pic05.jpg" alt="" />
+					</div>
+					<p>Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus ornare mi ut ante amet placerat aliquet. Volutpat eu sed ante lacinia sapien lorem accumsan varius montes viverra nibh in adipiscing blandit tempus accumsan.</p>
+					<p> Aenean iaculis, neque sed pretium egestas, nunc lacus tempus enim, nec tincidunt urna massa a libero. Aenean mattis bibendum est, a pharetra elit. Morbi commodo lectus quis blandit mattis. Cras pharetra quam quis tincidunt tempus. Donec a sem magna. Nullam purus purus, fermentum id lorem sit amet, porta elementum neque. Proin vulputate metus ac faucibus luctus.</p>
+					<p>Ut congue purus sed elit consectetur tempus. Duis convallis, quam quis pellentesque vestibulum, tellus arcu hendrerit ante, sed dictum felis nisl vitae magna. Integer et sapien a erat molestie tempor. Cras est odio, suscipit id porttitor id, mollis et ligula. Curabitur molestie mi molestie accumsan faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer porta malesuada pellentesque. Morbi imperdiet dictum velit, eu volutpat sem posuere non. Fusce ullamcorper gravida velit, sed sollicitudin libero iaculis id. Ut eu neque non odio fringilla faucibus nec quis neque. Quisque et nisi fermentum, tincidunt libero a, condimentum ligula. Quisque ultrices blandit lacinia. Nulla velit lorem, placerat nec eros ut, fermentum pharetra dolor. Maecenas arcu ipsum, mattis et suscipit sed, convallis nec lectus. Nulla facilisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; </p>
+				</div>
+			</section>
+			<!-- Contato -->
+				<div class="wrapper">
+		<div  class="inner">
+			<div class="align-center">
+				<!-- Default form contact -->
+				<form class="text-center border border-light p-5" action="#!">
+
+					<p class="h4 mb-4">Contato</p><br>
+
+					<!-- Name -->
+					<input type="text" id="defaultContactFormName" class="form-control mb-4" placeholder="Name"><br>
+
+					<!-- Email -->
+					<input type="email" id="defaultContactFormEmail" class="form-control mb-4" placeholder="E-mail"><br>
+
+
+					<!-- Message -->
+					<div class="form-group">
+						<textarea class="form-control rounded-0" id="exampleFormControlTextarea2" rows="3"
+							placeholder="Escreva o que quiser"></textarea>
+					</div><br>
+
+					<!-- Send button -->
+					<button class="btn btn-danger btn-block" type="submit">Enviar</button>
+
+				</form>
+				<!-- Default form contact -->
+			</div>
+		</div>
+	</div>
+
+	<!-- Footer -->
+	<footer id="footer">
+		<div class="inner">
+			<h2>Entre em contato conosco</h2>
+			<ul class="actions flex-item left">
+				<h3>Herculândia - SP</h3>
+				<li><span class="icon fa-phone"></span> (14) 3486-1846</li>
+				<li><span class="icon fa-envelope"></span> emailparacontato@outlook.com</li>
+				<li><span class="icon fa-map-marker"></span> Av. São Paulo, XXX - Centro, Herculândia - SP, 17650-000</li>
+			</ul>
+			<ul class="actions flex-item right">
+				<h3>Tupã - SP</h3>
+				<li><span class="icon fa-phone"></span> (14) 3496-1419</li>
+				<li><span class="icon fa-envelope"></span> emailparacontato@outlook.com</li>
+				<li><span class="icon fa-map-marker"></span> Av. Tabajaras, 1433 - Centro, Tupã - SP, 17601-020</li>
+			</ul>
+		</div><br>
+		<div class="copyright">
+			&copy; Desenvolvido por <br><img class="logofim" src="images/click.png" alt="">
+		</div>
+	</footer>
+
+	<!-- Scripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/jquery.scrolly.min.js"></script>
+	<script src="assets/js/skel.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
